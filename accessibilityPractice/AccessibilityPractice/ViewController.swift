e0306b6e68b23e611f31e98a8e26c797a7f7864e
Katherine Owens <katherineowens@Katherines-MacBook-Pro.local>
2019-03-21 12:20:57 -0400

    fixed storyboard, added viewControllers and UIElements

---
+++ accessibilityPractice/AccessibilityPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccessibilityPractice
+//
+//  Created by Katherine Owens on 3/20/19.
+//  Copyright © 2019 Katherine Owens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
